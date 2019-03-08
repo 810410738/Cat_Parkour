ece4a50065995d493ea0da68c4b61bbbe09fc2aa
jerry_zj <jerrylzj810410738@gmail.com>
2019-03-08 13:08:46 +0800

    两个场景

---
+++ Assets/script/SozControl.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SozContorl : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        print("dsf");
+        Destroy(gameObject);
+        AudioManager.Instance.PlaySound("eat");
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+       // if (collision.tag == "player")
+        //{
+            print("dsf");
+            Destroy(gameObject);
+            AudioManager.Instance.PlaySound("eat");
+       // }
+    }
+}
---
+++ Assets/script/background_move.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class background_move : MonoBehaviour
+{
+    public float speed = 0.2f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 v = transform.localPosition;
+        v.x -= speed * Time.deltaTime;
+        if (v.x < -18.1f)
+        {
+            v.x += 18.1f * 2;
+        }
+        transform.localPosition = v;
+    }
+
+}
